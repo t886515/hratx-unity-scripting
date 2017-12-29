e1832b000ffda1026b2a9852ac2fa7aa1ceb48ee
Charlie <ctbwilhite@gmail.com>
2017-12-28 21:42:48 -0600

    Added bird animations and scripts. Bird now flaps on mouse click, and dies when colliding with another object.

--- Floppy Bird/Assets/Scripts/Bird.cs
+++ Floppy Bird/Assets/Scripts/Bird.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour {
 
+	public float upForce = 200f;
+	private bool isDead = false;
+	private Rigidbody2D rb2d;
+	private Animator anim;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (isDead == false)
+		{
+			if (Input.GetMouseButtonDown (0))
+			{
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce(new Vector2(0, upForce));
+				anim.SetTrigger ("Flap");
+			}
+		}
+	}
+
+	void OnCollisionEnter2D ()
+	{
+		isDead = true;
+		anim.SetTrigger ("Die");
 	}
 }
