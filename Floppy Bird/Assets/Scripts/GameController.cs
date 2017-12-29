46399d59f9d728884a16eaeb0b3922cb836ff3b5
Charlie <ctbwilhite@gmail.com>
2017-12-28 23:43:37 -0600

    Added score count and column triggers to allow the player to score points when they make it through a column.

--- Floppy Bird/Assets/Scripts/GameController.cs
+++ Floppy Bird/Assets/Scripts/GameController.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
 	public GameObject gameOverText;
 	public bool gameOver;
+	public Text scoreText;
 	public static GameController instance;
     public float scrollSpeed = -1.5f;
+	private int score = 0;
 
     // Use this for initialization
     void Awake ()
@@ -32,6 +35,15 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
+	public void BirdScored ()
+	{
+		if (gameOver)
+		{
+			return;
+		}
+		score++;
+		scoreText.text = "Score: " + score.ToString ();
+	}
 	public void BirdDied ()
 	{
 		gameOverText.SetActive (true);
